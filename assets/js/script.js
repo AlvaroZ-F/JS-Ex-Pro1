d95ba964ea762a104e89b1e88ee27a0a09b7faa5
Álvaro Zambrana Fernández <56718986+AlvaroZ-F@users.noreply.github.com>
2020-03-27 01:24:19 +0100

    update..

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,19 @@ $(document).ready(function () {
 
 function getArticle(nArticle = 0) {
 	$.getJSON("posts.json", function (result) {
-		var output = "<div class='article'><div class='row'><div class='col-md-9'>";
+
+		var output = '';
 
 		$.each(result.POSTS, function (i, field) {
-			if (field.ID === nArticle.toString()) {
-
+			if (field.ID == nArticle.toString()) {
+				output += "<div class='article'><div class='row'><div class='col-md-9'><h3>" + field.Title + "</h3>";
+				output += "<p class='art-date'>" + field.Date + "</p></div>";
+				output += "<div class=col-md-3><button class='btn btn-primary'>" + field.NComments + " Comentarios</button></div></div>";
+				output += "<div class='article-content'><p>" + field.Content + "</p></div></div>";
 			}
-		}
+		});
+
+		$("#article-list").append(output);
+
 	});
 }
